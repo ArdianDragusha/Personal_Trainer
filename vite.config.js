3da25b0f03c77135f0659d72c3ba25ae902000cd
ArdianDragusha <ardian.dragusha@myy.haaga-helia.fi>
2025-05-07 16:49:14 +0300

    git actions vaihdettu

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  base: '/Personal_Trainer/',
 })
